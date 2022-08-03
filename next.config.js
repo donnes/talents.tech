99a7d79972405193bd18b7cd4d7a4a2a008edeb0
Donald Silveira <donaldsilveira@gmail.com>
2022-08-03 08:10:57 -0300

    chore: setup Tailwind CSS

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["tailwindui.com", "images.unsplash.com"],
+  },
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
